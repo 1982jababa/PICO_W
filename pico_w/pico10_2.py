a95e3d67c0c190b86781f5a1bf60d94e3b0e6c0c
1982jababa <98632320+1982jababa@users.noreply.github.com>
2023-12-16 16:00:34 +0800

    modify

--- pico_w/pico10_2.py
+++ pico_w/pico10_2.py
@@ -3,7 +3,7 @@ import time
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect('11T','0972830352')
+wlan.connect('11T','49038731115')
 
 while not wlan.isconnected() and wlan.status() >= 0:
     print("等待連線中....")
